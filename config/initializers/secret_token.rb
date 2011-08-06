0e3db44995a6e875999ab1f23c58569ef240531a
Rob Gough <robertgough@gmail.com>
2011-08-06 12:21:49 +0100

    Initial rails checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeedsHack2::Application.config.secret_token = 'eeeb657e551d2d6a2b097a9728648a2b538be2228cd1ec6e5c4721b54056cc6894a37dbeb09f9a9f97e86b809a03735dc17e541b5c5a8478f3d9b561c9d0e2ae'
